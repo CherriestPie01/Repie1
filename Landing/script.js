6bb204e42868343ec6c6369b78e19a508eed06b7
Светлана Каменская <107999937+CherriestPie01@users.noreply.github.com>
2022-07-18 21:27:15 +0300

    Add files via upload

---
+++ Landing/script.js
@@ -0,0 +1,13 @@
+let title = document.querySelector(".cake-bg-text");
+
+title.innerHTML = "Красота во вкусе";
+
+let sweets = document.querySelector(".sweets-menu")
+
+let button = document.querySelector(".nav-open");
+
+button.addEventListener("click", () => {
+    /*sweets.classList.add('opened')
+    alert(sweets.classList)*/
+    alert('Нажатие подтверждено')
+});
